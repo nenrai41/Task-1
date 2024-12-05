c2565c6d952d1cdc17b9b32e4185aa370f9a6d68
rob <rev8978@gmail.com>
2024-12-06 00:31:49 +0300

    Initial commit

---
+++ GarifullinRE/Program.cs
@@ -0,0 +1,25 @@
+﻿using GarifullinRE;
+try
+{
+    Student student1 = new Student("Иван", "Иванов", "Иванович", "М1О-103БВ-24", "123456", Course.First);
+    Student student2 = new Student("Петр", "Петров", "Петрович", "М2О-211БВ-23", "654321", Course.Second);
+
+    Console.WriteLine(student1);
+    Console.WriteLine();
+    Console.WriteLine(student2);
+
+    Console.WriteLine();
+    Console.WriteLine($"Один и тот же студент? {student1.Equals(student2)}");
+
+    Console.WriteLine();
+    HashSet<Student> students = new HashSet<Student>();
+    students.Add(student1);
+    students.Add(student2);
+
+    Student student1Dublicate = new Student("Иван", "Иванов", "Иванович", "М1О-103БВ-24", "123456", Course.First);
+    Console.WriteLine($"Добавлен ли дубликат студента? {students.Add(student1Dublicate)}");
+}
+catch(ArgumentNullException ex)
+{
+    Console.WriteLine($"Ошибка: {ex.Message}");
+}
---
+++ GarifullinRE/Student.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GarifullinRE
+{
+    public class Student : IEquatable<Student>
+    {
+        public Student(string firstName, string surname, string patronymic,string studyGroup, string recordBooksID, Course course)
+        {
+            FirstName = firstName ?? throw new ArgumentNullException(nameof(firstName), "Имя не может быть null.");
+            Surname = surname ?? throw new ArgumentNullException(nameof(surname), "Фамилия не может быть null.");
+            Patronymic = patronymic ?? throw new ArgumentNullException(nameof(patronymic), "Отчество не может быть null.");
+            StudyGroup = studyGroup ?? throw new ArgumentNullException(nameof(studyGroup), "Номер учебной группы не может быть null.");
+            RecordBooksID = recordBooksID ?? throw new ArgumentNullException(nameof(recordBooksID), "Номер зачетной книжки не может быть null.");
+
+        }
+        public override string ToString()
+        {
+            return $"ФИО студента: {Surname} {FirstName} {Patronymic} \nКурс: {Course} \nНомер учебной группы: {StudyGroup} \nНомер зачетной книжки: {RecordBooksID}";
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is Student otherStudent)
+            {
+                return Equals(otherStudent);
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Surname, FirstName, Patronymic, StudyGroup, RecordBooksID, Course);
+        }
+
+        public bool Equals(Student otherStudent)
+        {
+            if (otherStudent == null) return false;
+            return FirstName == otherStudent.FirstName &&
+                   Surname == otherStudent.Surname &&
+                   Patronymic == otherStudent.Patronymic &&
+                   StudyGroup == otherStudent.StudyGroup &&
+                   RecordBooksID == otherStudent.RecordBooksID &&
+                   Course == otherStudent.Course;
+        }
+
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public string StudyGroup { get; set; }
+        public string RecordBooksID { get; set; }
+
+        public Course Course;
+
+    }
+    public enum Course
+    {
+        First,
+        Second,
+        Third,
+        Fourth
+       
+    }
+}
